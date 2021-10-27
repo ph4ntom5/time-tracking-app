ae83eb7d7b9fa1c69f4f40ce3044cde3ab842f4b
ph4ntom5 <tomsstals1@gmail.com>
2021-10-27 15:54:59 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import "./App.css";
+import styled from "styled-components";
+import { GlobalStyles } from "./styles/global";
+
+const Wrapper = styled.section`
+  background-color: var(--bg);
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <GlobalStyles />
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Wrapper>
   );
 }
 
---
+++ src/styles/global.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyles = createGlobalStyle`
+
+html {
+    --bg: hsl(221, 47%, 9%);
+    --blue: hsl(246, 80%, 60%);
+    --work: hsl(15, 100%, 70%);
+    --study: hsl(348, 100%, 68%);
+    --excercise: hsl(145, 58%, 55%);
+    --social: hsl(264, 64%, 52%);
+    --self-care: hsl(43, 84%, 65%);
+    font-size:62.5%;
+}
+`;
