ba84b165f934a0e544a1c86397f37ce3df5b4269
ph4ntom5 <tomsstals1@gmail.com>
2021-11-01 15:50:37 +0100

    wip

--- src/components/user_nav.js
+++ src/components/user_nav.js
@@ -1,11 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import UserImage from "../images/image-jeremy.png";
 
 const Wrapper = styled.section`
   background-color: var(--nav);
   width: 80%;
-  height: 20rem;
+  height: 18rem;
   border-radius: 1rem;
+`;
+
+const WrapperText = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 1.5rem;
+
+  p {
+    font-size: 1.6rem;
+    opacity: 0.5;
+    color: var(--highlight);
+    padding: 0 2rem;
+    cursor: pointer;
+
+    &:active {
+      color: white;
+      opacity: 0.9;
+    }
+  }
 `;
 
 const UserBg = styled.section`
@@ -13,13 +34,56 @@ const UserBg = styled.section`
   height: 10rem;
   width: 100%;
   border-radius: 1rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+const UserInfo = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  h5 {
+    color: white;
+    font-size: 1.6rem;
+    opacity: 0.5;
+    text-align: left;
+    margin-left: 1rem;
+    font-weight: 400;
+  }
+  p {
+    color: white;
+    font-size: 2.4rem;
+    margin-top: -2.5rem;
+    margin-left: 1rem;
+    opacity: 0.8;
+  }
+`;
+
+const UserPhoto = styled.img`
+  width: 4.5rem;
+  height: 4.5rem;
+  margin: 0.5rem;
+  border: 2px solid #ffffff;
+  border-radius: 100rem;
 `;
 
 export default function userNav() {
   return (
     <>
       <Wrapper>
-        <UserBg />
+        <UserBg>
+          <UserPhoto src={UserImage} />
+          <UserInfo>
+            <h5>Report for</h5>
+            <p>Jeremy Robson</p>
+          </UserInfo>
+        </UserBg>
+        <WrapperText>
+          <p>Daily</p>
+          <p>Weekly</p>
+          <p>Monthly</p>
+        </WrapperText>
       </Wrapper>
     </>
   );
